49f0590dbb828d342ad46e006d3f6857926b4867
Jason Woods <jason@coder.haus>
2021-02-11 09:13:06 -0600

    Finished launchpad. Would though like to add the rest of the meshes/textures but hey. Included two ways to set the launch.

--- Source/stealth_assassin/Private/FPSLaunchPad.cpp
+++ Source/stealth_assassin/Private/FPSLaunchPad.cpp
@@ -2,33 +2,67 @@
 
 
 #include "FPSLaunchPad.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+//#include "GameFramework/Character.h"
+#include "FPSCharacter.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values for this component's properties
-UFPSLaunchPad::UFPSLaunchPad()
+AFPSLaunchPad::AFPSLaunchPad()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	LaunchPadComp = CreateDefaultSubobject<UBoxComponent>(TEXT("LaunchPadComp"));
+	LaunchPadComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	LaunchPadComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	LaunchPadComp->SetCollisionResponseToChannel(ECC_PhysicsBody, ECR_Overlap);
+	LaunchPadComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	//LaunchPadComp->SetBoxExtent(FVector(200.0f));
+	RootComponent = LaunchPadComp;
+
+	//LaunchPadComp->SetHiddenInGame(false);
+	LaunchPadComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSLaunchPad::HandleOverlap);
+
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	MeshComp->SetupAttachment(RootComponent);
+
+	LaunchVelocity = 1000;
 }
 
 
-// Called when the game starts
-void UFPSLaunchPad::BeginPlay()
+// We're going to launch either the pawn, or an actor if it's moved into the zone
+// But we have to handle those differently.  We can launch a pawn, but have to apply an impulse to an actor.
+void AFPSLaunchPad::HandleOverlap(UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	Super::BeginPlay();
+	// Another way to get this We would need a LaunchAngle param
+	//FRotator LaunchDirection = GetActorRotation();
+	//LaunchDirection.Pitch = LaunchAngle;
+	//FVector LaunchVector = LaunchDirection.Vector() * LaunchVelocity;
 
-	// ...
-	
-}
+	FVector forwardVector = this->GetActorForwardVector();
+	FVector upVector = this->GetActorUpVector();
+
+	// We need a forward/upwards vector to launch the player in
+	FVector launchVector = forwardVector.operator+(upVector).operator*(LaunchVelocity);
+
+	AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
+	if (MyPawn != nullptr)
+	{
+		MyPawn->LaunchCharacter(launchVector, true, true);
+
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ActivateLaunchPadEffect, GetActorLocation());
+	}
+	else if (OtherComp && OtherComp->IsSimulatingPhysics())
+	{
+		// We need to use this as a change in velocity.
+		OtherComp->AddImpulse(launchVector, NAME_None, true);
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ActivateLaunchPadEffect, GetActorLocation());
+	}
 
 
-// Called every frame
-void UFPSLaunchPad::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
 }
 
